97184103c17dcab7c02f91cbbc843acb1cfaf0e8
FaiqRustamov <faiq.rustamov.98@mail.ru>
2021-02-14 17:32:52 +0400

    input ad and bug fix

--- assets/js/register.js
+++ assets/js/register.js
@@ -69,3 +69,14 @@ function fixStepIndicator(n) {
   //... and adds the "active" class on the current step:
   x[n].className += " active";
 }
+$('input[type="date"], input[type="datetime"], input[type="datetime-local"], input[type="month"], input[type="time"], input[type="week"]').each(function() {
+  var el = this, type = $(el).attr('type');
+  if ($(el).val() == '') $(el).attr('type', 'text');
+  $(el).focus(function() {
+      $(el).attr('type', type);
+      el.click();
+  });
+  $(el).blur(function() {
+      if ($(el).val() == '') $(el).attr('type', 'text');
+  });
+});
